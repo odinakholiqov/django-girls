0c1fbd6cc21f67e5a6415d65e0fe472c7148928d
odinakholiqov <bobojon.kholikov@gmail.com>
2024-04-25 14:09:54 +0500

    fixes

--- softclub/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SoftclubConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "softclub"
--- test.py
+++
@@ -1,5 +0,0 @@
-first_tuple = (1, 2, 3, 4, 5)
-second_tuple = (2, 4, 5)
-
-contains_all = (elem in first_tuple for elem in second_tuple)
-print(contains_all) # True
